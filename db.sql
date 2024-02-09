dfa575b1c240861d4075bb6f2e4b657f43e54ebf
Elementrix08 <verushannaidoo@gmail.com>
2024-02-09 11:46:49 +0200

    Updated database scheme, added constraints and default values

--- db.sql
+++ db.sql
@@ -15,22 +15,20 @@ CREATE TABLE [dbo].[StaffCode] (
 );
 GO
 
-CREATE TABLE [dbo].[Staff] (
-    [StaffID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    [StaffCode] [int] NOT NULL,
+CREATE TABLE [dbo].[Team] (
+    [TeamID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
     [Name] [varchar](120) NOT NULL,
-    [Surname] [varchar](120) NOT NULL,
-
-    FOREIGN KEY (StaffCode) REFERENCES [dbo].StaffCode(StaffCodeID)
 );
 GO
 
-CREATE TABLE [dbo].[Team] (
-    [TeamID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    [StaffID] [int] NOT NULL,
+CREATE TABLE [dbo].[Staff] (
+    [StaffID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    [StaffCode] [int] NOT NULL,
+    [TeamID] [int] NOT NULL,
     [Name] [varchar](120) NOT NULL,
+    [Surname] [varchar](120) NOT NULL,
 
-    FOREIGN KEY (StaffID) REFERENCES [dbo].Staff(StaffID)
+    FOREIGN KEY (TeamID) REFERENCES [dbo].Team(TeamID)
 );
 GO
 
@@ -43,11 +41,15 @@ GO
 CREATE TABLE [dbo].[Player] (
     [PlayerID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
     [PositionID] [int] NOT NULL, 
+    [TeamID] [int] NOT NULL,
     [Name] [varchar](120) NOT NULL,
     [Surname] [varchar](120) NOT NULL,
-    [DateOfBirth] [datetime2] NULL,
+    [DateOfBirth] [date] NULL,
 
-    FOREIGN KEY (PositionID) REFERENCES [dbo].PositionCode(PositionID)
+    FOREIGN KEY (PositionID) REFERENCES [dbo].PositionCode(PositionID),
+    FOREIGN KEY (TeamID) REFERENCES [dbo].Team(TeamID),
+
+    CONSTRAINT ChkDatePast CHECK ([DateOfBirth] < GETDATE() OR [DateOfBirth] IS NULL)
 );
 GO
 
@@ -64,10 +66,13 @@ GO
 
 CREATE TABLE [dbo].[Result] (
     [ResultID] [int] PRIMARY KEY NOT NULL,
-    [GoalsFor] [int] NOT NULL, 
-    [GoalsAgainst] [int] NOT NULL,
+    [GoalsFor] [int] NOT NULL DEFAULT 0, 
+    [GoalsAgainst] [int] NOT NULL DEFAULT 0,
 
-    FOREIGN KEY (ResultID) REFERENCES [dbo].Fixture(FixtureID)
+    FOREIGN KEY (ResultID) REFERENCES [dbo].Fixture(FixtureID),
+
+    CONSTRAINT ChkPositiveGoalsFor CHECK ([GoalsFor] >= 0),
+    CONSTRAINT ChkPositiveGoalsAgainst CHECK ([GoalsAgainst] >= 0)
 );
 GO
 
@@ -75,15 +80,25 @@ CREATE TABLE [dbo].[PlayerFixtureStats] (
     [PlayerFixtureStatsID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
     [PlayerID] [int] NOT NULL, 
     [FixtureID] [int] NOT NULL, 
-    [Saves] [int] NOT NULL,
-    [Assists] [int] NOT NULL,
-    [Goals] [int] NOT NULL,
-    [Tackles] [int] NOT NULL,
-    [YellowCards] [int] NOT NULL,
-    [RedCards] [int] NOT NULL,
-    [Fouls] [int] NOT NULL,
+    [Saves] [int] NOT NULL DEFAULT 0,
+    [Assists] [int] NOT NULL DEFAULT 0,
+    [Goals] [int] NOT NULL DEFAULT 0,
+    [Tackles] [int] NOT NULL DEFAULT 0,
+    [YellowCards] [int] NOT NULL DEFAULT 0,
+    [RedCards] [int] NOT NULL DEFAULT 0,
+    [Fouls] [int] NOT NULL DEFAULT 0,
+    [GameTimeInMinutes] [int] NOT NULL DEFAULT 0,
 
     FOREIGN KEY (PlayerID) REFERENCES [dbo].Player(PlayerID),
-    FOREIGN KEY (FixtureID) REFERENCES [dbo].Fixture(FixtureID)
+    FOREIGN KEY (FixtureID) REFERENCES [dbo].Fixture(FixtureID),
+
+    CONSTRAINT ChkPositiveSaves CHECK ([Saves] >= 0),
+    CONSTRAINT ChkPositiveAssists CHECK ([Assists] >= 0),
+    CONSTRAINT ChkPositiveGoals CHECK ([Goals] >= 0),
+    CONSTRAINT ChkPositiveTackles CHECK ([Tackles] >= 0),
+    CONSTRAINT ChkPositiveYellowCards CHECK ([YellowCards] >= 0),
+    CONSTRAINT ChkPositiveRedCards CHECK ([RedCards] >= 0),
+    CONSTRAINT ChkPositiveFouls CHECK ([Fouls] >= 0),
+    CONSTRAINT ChkPositiveGameTime CHECK ([GameTimeInMinutes] >= 0)
 );
 GO
