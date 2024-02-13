7efd19ae08aacd5705b942510d48d8a79eea3b16
Elementrix08 <verushannaidoo@gmail.com>
2024-02-13 20:23:46 +0200

    Added UdfGetMaxGoalCount sql to computer maximum goal for a day

---
+++ scripts/goal-count-proc.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION UdfGetMaxGoalCount (@Date DATE) 
+RETURNS int 
+AS 
+BEGIN 
+    DECLARE @maxValue int;
+
+    SELECT @maxValue = CASE
+        WHEN MAX(Result.[GoalsFor]) > MAX(Result.[GoalsAgainst]) THEN MAX(Result.[GoalsFor])
+        ELSE MAX(Result.[GoalsAgainst])
+        END
+        FROM
+        Result
+        LEFT JOIN Fixture ON Fixture.[FixtureID] = Result.[FixtureID]
+        WHERE CONVERT(DATE, Fixture.[Date]) = CONVERT(DATE, @Date);
+
+    RETURN @maxValue;
+END;
