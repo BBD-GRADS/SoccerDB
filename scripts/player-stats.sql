4387feed61d68a7d1752a28ff9ce21bdd7607227
Elementrix08 <verushannaidoo@gmail.com>
2024-02-13 14:08:01 +0200

    Add player season stats procedure

--- scripts/player-stats.sql
+++ scripts/player-stats.sql
@@ -1,8 +1,12 @@
-USE SoccerDB;
+--liquibase formatted sql
+--changeset verushan:2
 
+CREATE PROCEDURE ProcGetSeasonScore
+AS
 SELECT
     Player.[PlayerID],
     Player.[Name],
+    Player.[Surname],
     (
         SUM(PlayerFixtureStats.[Assists]) + 
         SUM(PlayerFixtureStats.[Goals]) +
@@ -10,8 +14,11 @@ SELECT
         SUM(PlayerFixtureStats.[YellowCards]) +
         SUM(PlayerFixtureStats.[RedCards]) -
         SUM(PlayerFixtureStats.[Fouls])
-    ) AS [SeasonScore]
+    ) AS [Season Score]
 FROM
     Player
-    LEFT JOIN PlayerFixtureStats ON Player.[PlayerID] = PlayerFixtureStats.[PlayerID]
-GROUP BY Player.[PlayerID], Player.[Name]
+    LEFT JOIN PlayerFixtureStats ON PlayerFixtureStats.[PlayerID] = Player.[PlayerID]
+    LEFT JOIN Fixture ON PlayerFixtureStats.[FixtureID] = Fixture.[FixtureID]
+WHERE YEAR(Fixture.[Date]) = YEAR(GETDATE())
+GROUP BY Player.[PlayerID], Player.[Name], Player.[Surname]
+ORDER BY [Season Score] DESC;
