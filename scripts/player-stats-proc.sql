8dec155b8325f51cb3738d8fefe151f9d7403dbf
Elementrix08 <verushannaidoo@gmail.com>
2024-02-13 16:25:33 +0200

    Formatted player season score sql

--- scripts/player-stats-proc.sql
+++ scripts/player-stats-proc.sql
@@ -1,24 +1,28 @@
 --liquibase formatted sql
 --changeset verushan:2
 
-CREATE PROCEDURE ProcGetSeasonScore
-AS
+CREATE PROCEDURE ProcGetSeasonScore AS
 SELECT
     Player.[PlayerID],
     Player.[Name],
     Player.[Surname],
     (
         SUM(PlayerFixtureStats.[Assists]) + 
-        SUM(PlayerFixtureStats.[Goals]) +
-        SUM(PlayerFixtureStats.[Tackles]) +
-        SUM(PlayerFixtureStats.[YellowCards]) +
-        SUM(PlayerFixtureStats.[RedCards]) -
+        SUM(PlayerFixtureStats.[Goals]) + 
+        SUM(PlayerFixtureStats.[Tackles]) + 
+        SUM(PlayerFixtureStats.[YellowCards]) + 
+        SUM(PlayerFixtureStats.[RedCards]) - 
         SUM(PlayerFixtureStats.[Fouls])
     ) AS [Season Score]
 FROM
     Player
     LEFT JOIN PlayerFixtureStats ON PlayerFixtureStats.[PlayerID] = Player.[PlayerID]
     LEFT JOIN Fixture ON PlayerFixtureStats.[FixtureID] = Fixture.[FixtureID]
-WHERE YEAR(Fixture.[Date]) = YEAR(GETDATE())
-GROUP BY Player.[PlayerID], Player.[Name], Player.[Surname]
-ORDER BY [Season Score] DESC;
+WHERE
+    YEAR(Fixture.[Date]) = YEAR(GETDATE())
+GROUP BY
+    Player.[PlayerID],
+    Player.[Name],
+    Player.[Surname]
+ORDER BY
+    [Season Score] DESC;
