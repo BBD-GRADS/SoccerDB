771cd652225ee9a98638208e83b2fe1be807bb3c
Elementrix08 <verushannaidoo@gmail.com>
2024-02-14 17:51:54 +0200

    Moved Result table trigger into database schema

--- scripts/db.sql
+++ scripts/db.sql
@@ -90,3 +90,31 @@ CREATE TABLE [dbo].[PlayerFixtureStats] (
     CONSTRAINT ChkPositiveFouls CHECK ([Fouls] >= 0),
     CONSTRAINT ChkPositiveGameTime CHECK ([GameTimeInMinutes] >= 0)
 );
+
+--changeset verushan:2
+CREATE TRIGGER TrgResultsInsert ON Result INSTEAD OF
+INSERT
+    AS BEGIN IF EXISTS (
+        SELECT
+            1
+        FROM
+            Inserted InsertedResult
+            INNER JOIN Fixture ON InsertedResult.FixtureID = Fixture.FixtureID
+        WHERE
+            Fixture.Date >= GETDATE()
+    ) BEGIN
+    RAISERROR (
+        'Cannot insert the result for a fixture in the future.', 1, 1 
+    );
+END
+ELSE BEGIN
+INSERT INTO
+    Result (FixtureID, GoalsFor, GoalsAgainst)
+SELECT
+    I.FixtureID,
+    I.GoalsFor,
+    I.GoalsAgainst
+FROM
+    Inserted I
+END
+END;
